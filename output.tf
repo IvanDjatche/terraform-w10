824099a17d697adb1e2a2923694e95cd04b9c277
ivanDjatche <ivandjatche90@gmail.com>
2024-05-14 09:48:13 +0100

    update output.tf

--- output.tf
+++ output.tf
@@ -2,25 +2,25 @@ output "vpc_id" {
   value = aws_vpc.vpc1.id
 }
 
-output "security_group_id" {
+output "sg1_security_group_id" {
   value = aws_security_group.ec2.id
 }
-output "security_group_id" {
+output "sg2_security_group_id" {
   value = aws_security_group.lb.id
 }
 
-output "instance_id" {
+output "web1-instance_id" {
   value = aws_instance.ec2-one.id
 }
 
-output "instance_id" {
+output "web2_instance_id" {
   value = aws_instance.ec2-two.id
 }
 
-output "publici_id" {
+output "web1_public_id" {
   value = aws_instance.ec2-one.public_ip
 }
-output "public_id" {
+output "web2_public_id" {
   value = aws_instance.ec2-two.public_ip
 }
 
